1f82da0c70350660a9fd81ee392971881caad6ed
hnigdelioglu <96379904+hnigdelioglu@users.noreply.github.com>
2022-03-21 22:56:29 -0400

    monday first commit

--- force-app/main/default/lwc/story10/story10.js
+++ force-app/main/default/lwc/story10/story10.js
@@ -1,5 +1,20 @@
-import { LightningElement } from 'lwc';
-
+import { api, LightningElement, wire } from 'lwc';
+import fetchCusTypeLocal from '@salesforce/apex/story10.fetchCusType'
 export default class Story10 extends LightningElement {
+
+    @api recordId;
+    customerOptions;
+    @wire(fetchCusTypeLocal, {
+        fdId: '$recordId'
+    })wiredData({error,data}){
+        if(data){
+            let option = [];
+            option.push({label:data.Customer_Type__c, value:data.Customer_Type__c})
+            this.customerOptions=option;
+            console.log('Option is '+JSON.stringify(this.customerOptions))
+        }
+    }
+
+
 
 }
